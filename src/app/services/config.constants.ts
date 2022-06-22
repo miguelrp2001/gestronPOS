788a519e6166716f0bbebb32dc4489b217600198
Miguel Román Párraga <mromparb@g.educaand.es>
2022-06-23 00:13:24 +0200

    443

--- src/app/services/config.constants.ts
+++ src/app/services/config.constants.ts
@@ -2,7 +2,7 @@
 // export const APIIP = "192.168.1.251";
 export const APIIP = "api.mijikonetwork.com";
 // export const PORT = "8000";
-export const PORT = "80";
+export const PORT = "443";
 // export const APIURL = "http://" + APIIP + ":" + PORT;
 export const APIURL = "https://" + APIIP + ":" + PORT;
 export const SECRET_KEY = "*&0M9ysI@hwNj0X#q#QG3c&rJyJIec2e!j01mJ*t4O$$*$KLEo&b79UPxOnvFSGwQ9BUTjdb4pz24WKAM#$TwNT@3kl1j%S$q1$8dSyZ47pL74f0x9XWA!3$8i67bD15";
